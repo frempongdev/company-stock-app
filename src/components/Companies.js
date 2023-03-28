6ab5d48327a0076889be5d5842aa3dffbd9ad7bc
Frempong <samankwaa.frempong@gmail.com>
2023-03-28 20:25:16 +0000

    Add filteredCompanies as a filter for company search

--- src/components/Companies.js
+++ src/components/Companies.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CompanyList from './CompanyList';
 
-const Companies = () => (
-  <div className="main">
-    <h5 className="header">World&rsquo;s Most Valuable Company Stocks</h5>
-    <input type="text" className="search" placeholder="Search..." />
-    <div className="company-box">
-      <CompanyList />
+const Companies = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return (
+    <div className="main">
+      <h5 className="header">World&rsquo;s Most Valuable Company Stocks</h5>
+      <input type="text" className="search" placeholder="Search..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+      <div className="company-box">
+        <CompanyList searchTerm={searchTerm} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Companies;
